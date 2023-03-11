46d24bbfe975e06b7023b8427991ca00946d3349
ab-dalton <73718652+ab-dalton@users.noreply.github.com>
2023-03-10 19:29:51 -0500

    Update speed_histograms.py

--- speed_histograms.py
+++ speed_histograms.py
@@ -10,7 +10,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import matplotlib as mpl
 
-
 # -----------------------------------------------------------------------------
 # Load database
 # -----------------------------------------------------------------------------
@@ -20,6 +19,8 @@ df = pd.read_csv('D:/Abby/paper_2/Iceberg Beacon Database-20211026T184427Z-001/I
 
 # Convert to datetime
 df["datetime_data"] = pd.to_datetime(df["datetime_data"].astype(str), format="%Y-%m-%d %H:%M:%S")
+
+path_figures = "D:/Abby/paper_2/plots/"
 
 
 #Create year, month, day columns
@@ -43,54 +44,165 @@ seasons = {12: 'Winter',
 
 df['Season'] = df['month'].apply(lambda x: seasons[x])
 
-for season in sorted(set(seasons.values())):
-    fig, ax = plt.subplots(figsize=(6, 4), constrained_layout=True)
-    params = {'mathtext.default': 'regular' }   
-    plt.rcParams.update(params)
-    font = {'size'   : 12,
-            'weight' : 'normal'}
-    mpl.rc('font', **font)
-    ax.set(xlabel="Speed (m $s^{-1}$)", ylabel="Count")
-    sns.histplot(
-        x="speed_ms", data=df[df['Season']==season], binwidth=0.1, color="grey", edgecolor="black", ax=ax
-    )
-    plt.title(season)
-    plt.show()
+winter = df.loc[df['Season'] == "Winter"]
+spring = df.loc[df['Season'] == "Spring"]
+summer = df.loc[df['Season'] == "Summer"]
+fall = df.loc[df['Season'] == "Fall"]
+
+
+
+
+
+# params = {'mathtext.default': 'regular' }
+# plt.rcParams.update(params)
+# font = {'size'   : 12,
+#         'weight' : 'normal'}
+# mpl.rc('font', **font)
+fig, axs = plt.subplots(2,2, figsize=(6, 4), constrained_layout=True, sharey=True, sharex=True)
+
+sns.histplot(x="speed_ms",
+             data=winter,
+             binwidth=0.2,
+             color="grey",
+             edgecolor="black",
+             ax=axs[0,0])
+axs[0,0].set(xlabel=None, ylabel=None)
+axs[0,0].annotate('A', (1, 1),
+                    xytext=(-5,-5),
+                    xycoords='axes fraction',
+                    textcoords='offset points',
+                    ha='right', va='top',
+                    fontsize=14)
+
+sns.histplot(x="speed_ms",
+             data=spring,
+             binwidth=0.2,
+             color="grey",
+             edgecolor="black",
+             ax=axs[0,1])
+axs[0,1].set(xlabel=None, ylabel=None)
+axs[0,1].annotate('B', (1, 1),
+                    xytext=(-5,-5),
+                    xycoords='axes fraction',
+                    textcoords='offset points',
+                    ha='right', va='top',
+                    fontsize=14)
+
+sns.histplot(x="speed_ms",
+             data=summer,
+             binwidth=0.2,
+             color="grey",
+             edgecolor="black",
+             ax=axs[1,0])
+axs[1,0].set(xlabel=None, ylabel=None)
+axs[1,0].annotate('C', (1, 1),
+                    xytext=(-5,-5),
+                    xycoords='axes fraction',
+                    textcoords='offset points',
+                    ha='right', va='top',
+                    fontsize=14)
+
+sns.histplot(x="speed_ms",
+             data=fall,
+             binwidth=0.2,
+             color="grey",
+             edgecolor="black",
+             ax=axs[1,1])
+axs[1,1].set(xlabel=None, ylabel=None)
+axs[1,1].annotate('D', (1, 1),
+                    xytext=(-5,-5),
+                    xycoords='axes fraction',
+                    textcoords='offset points',
+                    ha='right', va='top',
+                    fontsize=14)
+
+fig.text(0.5, -0.03, 'Speed (m $s^{-1}$)', ha='center', va='center', fontsize=12)
+fig.text(-0.03, 0.5, 'Count', ha='center', va='center', rotation='vertical', fontsize=12)
+
+
+# Save figure
+fig.savefig(
+    path_figures + "seasonal_speed.png",
+    dpi=300,
+    transparent=False,
+    bbox_inches="tight",
+)
+
+
+
+
+
+
+
+
+# # Create a 2x2 grid of subplots
+# fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(10, 8), constrained_layout=True)
+
+# # Set plot parameters
+# params = {'mathtext.default': 'regular' }   
+# plt.rcParams.update(params)
+# font = {'size': 12, 'weight': 'normal'}
+# mpl.rc('font', **font)
+
+# # Loop over the seasons and plot each histogram in a separate subplot
+# for i, season in enumerate(['Winter', 'Spring', 'Summer', 'Fall']):
+#     row = i // 2
+#     col = i % 2
+#     ax = axes[row, col]
+#     ax.set(xlabel="Speed (m $s^{-1}$)", ylabel="Count")
+#     sns.histplot(x="speed_ms", data=df[df['Season'] == season], binwidth=0.1, color="grey", edgecolor="black", ax=ax)
+#     # ax.set_title(season, loc='center')
+#     ax.set(ylim=(0,18000))
+#     ax.set(xlim=(0,None))
+#     ax.xaxis.set_major_locator(ticker.MultipleLocator(0.5))
+
+# # Add space between subplots
+# plt.tight_layout()
+# # Show the plot
+# plt.show()
+
+# # Save figure
+# fig.savefig(
+#     path_figures + "seasonal_speed.png",
+#     dpi=300,
+#     transparent=False,
+#     bbox_inches="tight",
+# )
+
+
+
+
+
+
+
+
+
+# for season in sorted(set(seasons.values())):
+#     fig, ax = plt.subplots(figsize=(6, 4), constrained_layout=True)
+#     params = {'mathtext.default': 'regular' }   
+#     plt.rcParams.update(params)
+#     font = {'size'   : 12,
+#             'weight' : 'normal'}
+#     mpl.rc('font', **font)
+#     ax.set(xlabel="Speed (m $s^{-1}$)", ylabel="Count")
+#     sns.histplot(
+#         x="speed_ms", data=df[df['Season']==season], binwidth=0.1, color="grey", edgecolor="black", ax=ax
+#     )
+#     plt.title(season)
+#     plt.show()
     
 
-# Create a 2x2 grid of subplots
-fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(10, 8), constrained_layout=True)
-
-# Set plot parameters
-params = {'mathtext.default': 'regular' }   
-plt.rcParams.update(params)
-font = {'size': 12, 'weight': 'normal'}
-mpl.rc('font', **font)
-
-# Loop over the seasons and plot each histogram in a separate subplot
-for i, season in enumerate(['Winter', 'Spring', 'Summer', 'Fall']):
-    row = i // 2
-    col = i % 2
-    ax = axes[row, col]
-    ax.set(xlabel="Speed (m $s^{-1}$)", ylabel="Count")
-    sns.histplot(x="speed_ms", data=df[df['Season'] == season], binwidth=0.05, color="grey", edgecolor="black", ax=ax)
-    ax.set_title(season, loc='center')
-
-# Add space between subplots
-plt.tight_layout()
-# Show the plot
-plt.show()
 
 
-fig, ax = plt.subplots(figsize=(6, 4), constrained_layout=True)
-params = {'mathtext.default': 'regular' }
-plt.rcParams.update(params)
-font = {'size'   : 12,
-        'weight' : 'normal'}
-mpl.rc('font', **font)
-ax.set(xlabel="Speed (m $s^{-1}$)", ylabel="Count")
-sns.histplot(x="speed_ms", data=df, binwidth=0.1, color="grey", edgecolor="black", ax=ax)
-plt.show()
+# fig, ax = plt.subplots(figsize=(6, 4), constrained_layout=True)
+# params = {'mathtext.default': 'regular' }
+# plt.rcParams.update(params)
+# font = {'size'   : 12,
+#         'weight' : 'normal'}
+# mpl.rc('font', **font)
+# ax.set(xlabel="Speed (m $s^{-1}$)", ylabel="Count")
+# sns.histplot(x="speed_ms", data=df, binwidth=0.1, color="grey", edgecolor="black", ax=ax)
+# plt.show()
 
 
 
